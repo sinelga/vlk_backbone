59987a2e729233c51d4e19a709db4346c794e989
alex <support@mazurov.eu>
2016-02-16 17:31:18 +0200

    Initial dist subtree commit

---
+++ version_desk_react_00/webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+
+module.exports = {
+                entry: {
+                    app: [ './src/app.js'],
+                        vendor: ["react","react-router","react-bootstrap","react-document-meta"],
+                  },
+   output: {
+     path: __dirname + '/dist/',
+     filename: 'deskvlkbundle.js',
+     publicPath: '/'
+   },
+   module: {
+     loaders: [
+       { test: /\.js$/, loader: 'babel', exclude: [/node_modules/]  }
+      ]
+   },
+   
+   plugins: [
+             new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.deskvlkbundle.js")
+           ]
+};
