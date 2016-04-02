d7743be3a068393f55cf5f2067f8ddacc9780ba8
juno <juno@krasava.fi>
2016-04-02 06:14:46 +0300

    dev

--- version_desk_react_00/src/app.js
+++ version_desk_react_00/src/app.js
@@ -2,7 +2,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
-//import stubbedCourses from './stubs/REAL_ESTATE_OBJS'
 
 const rootRoute = {
   component: 'div',
